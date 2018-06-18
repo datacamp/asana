390d99b80df83a42c8a9010cc673dae1a7663c2e
Richard Cotton <richierocks@gmail.com>
2018-06-18 12:15:10 -0400

    add fix_ids()

--- R/call-api.R
+++ R/call-api.R
@@ -21,3 +21,19 @@ call_asana_api <- function(endpoint, access_token, ...) {
   txt <- content(response, as = "text")
   fromJSON(txt)
 }
+
+#' Fix IDs
+#'
+#' Asana stores IDs as integers. Many of these integers are greater than the
+#' values that R can store, \code{.Machine$integer.max}. That causes
+#' \code{jsonlite} to convert them to \code{numeric}. This function undo that.
+#' @param id A numeric vector.
+#' @return A character vector.
+#' @seealso \code{\link[base]{format}}
+#' @examples
+#' id <- jsonlite::fromJSON("1234567890123450")
+#' fix_ids(id)
+#' @noRd
+fix_ids <- function(id) {
+  format(id, digits = 22)
+}
