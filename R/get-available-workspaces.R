a9627feabdf750825c3b8844877763fe40113ddd
chriscardillo <chris.cardillo@datacamp.com>
2018-06-21 18:14:39 -0400

    Created two functions for getting projects, still need to make one for fetching team-specific projects. Also changed the name of the .R file for getting available workspaces. In short, naming my .R files based on specific pages of the API documentation.

--- R/get-available-workspaces.R
+++ R/get-available-workspaces.R
@@ -6,11 +6,11 @@
 #' @examples
 #' \donttest{
 #' ## Marked as don't test because an access token is needed
-#' get_asana_workspaces()
+#' get_all_workspaces()
 #' }
 #' @export
 
-get_asana_workspaces <- function(access_token = Sys.getenv("ASANA_ACCESS_TOKEN")){
+get_all_workspaces <- function(access_token = Sys.getenv("ASANA_ACCESS_TOKEN")){
 
   results <- call_asana_api("workspaces", access_token = access_token)
 
---
+++ R/query-for-projects.R
@@ -0,0 +1,46 @@
+#' List all available projects in Asana
+#'
+#' @param access_token An Asana access token.
+#' @return A list containing a dataframe of all available projects to the Asana user.
+#' @references \url{https://asana.com/developers/api-reference/projects#query}
+#' @examples
+#' \donttest{
+#' ## Marked as don't test because an access token is needed
+#' get_all_projects()
+#' }
+#' @export
+
+get_all_projects <- function(access_token = Sys.getenv("ASANA_ACCESS_TOKEN")){
+
+  results <- call_asana_api("projects", access_token = access_token)
+
+  results$data <- fix_ids(results$data)
+
+  results
+
+}
+
+#' List a specific workspace's projects in Asana
+#'
+#' @param workspace_id An Asana workspace ID
+#' @param access_token An Asana access token.
+#' @return A list containing a dataframe of all projects available in a specific workspace
+#' @references \url{https://asana.com/developers/api-reference/projects#query}
+#' @examples
+#' \donttest{
+#' ## Marked as don't test because an access token is needed
+#' get_workspace_projects()
+#' }
+#' @export
+
+get_workspace_projects <- function(workspace_id, access_token = Sys.getenv("ASANA_ACCESS_TOKEN")){
+
+  endpoint <- paste0("workspaces/", workspace_id, "/projects")
+
+  results <- call_asana_api(endpoint, access_token = access_token)
+
+  results$data <- fix_ids(results$data)
+
+  results
+
+}
